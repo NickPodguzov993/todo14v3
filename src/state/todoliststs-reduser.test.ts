1acc6aafcb8b900cb582e99180333318dce80945
NickPodguzov993 <verytallman993@gmail.com>
2024-02-05 15:30:39 +0300

    Initial commit

---
+++ src/state/todoliststs-reduser.test.ts
@@ -0,0 +1,7 @@
+import {changeTodolistFilterAC, FilterValuesType} from "./todolists-reducer";
+
+test('todolists should be set to the state'), () => {
+    let newFilter: FilterValuesType = 'completed'
+
+
+}
